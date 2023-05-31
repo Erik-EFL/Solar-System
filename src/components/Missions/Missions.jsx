10935623b76768e0cdbdd4c31f4b547033e02bb6
erik lima <erik.lima.f@outlook.com>
2023-05-31 01:26:01 -0300

    feat:

--- src/components/Missions/Missions.jsx
+++ src/components/Missions/Missions.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import Title from '../Title';
 import missions from '../../data/missions';
-import MissionCard from './MissionCard';
+import Title from '../Title';
 import '../styles/Missions.css';
+import MissionCard from './MissionCard';
 
 class Missinons extends React.Component {
   render() {
     return (
-      <div className='missions' data-testid="missions">
+      <div className="missions" data-testid="missions">
         <Title headline="Missões" />
         <div className="mission">
-          {
-            missions.map((mission) => (
-              <MissionCard
-                key={mission.name}
-                name={mission.name}
-                year={mission.year}
-                country={mission.country}
-                destination={mission.destination}
-              />
-            ))
-          }
+          {missions.map((mission) => (
+            <MissionCard
+              key={mission.name}
+              name={mission.name}
+              year={mission.year}
+              country={mission.country}
+              destination={mission.destination}
+            />
+          ))}
         </div>
       </div>
     );
